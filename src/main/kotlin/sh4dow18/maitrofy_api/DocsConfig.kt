b5b7a23f0a95f0443536fee3c908828a415b38ce
sh4dow18 <ramses.solano.arias@gmail.com>
2025-08-27 22:47:22 -0600

    add: custom open api function
    Add Custom Open API in Swagger Config in Docs File

--- src/main/kotlin/sh4dow18/maitrofy_api/DocsConfig.kt
+++ src/main/kotlin/sh4dow18/maitrofy_api/DocsConfig.kt
@@ -3,7 +3,13 @@ package sh4dow18.maitrofy_api
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.info.Info
 import io.swagger.v3.oas.annotations.tags.Tag
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.media.Schema
+import org.springdoc.core.customizers.OpenApiCustomizer
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.util.TreeMap
+
 // Set Open API Definition
 @OpenAPIDefinition(
     info = Info(title = "Maitrofy API", version = "0.10.0"),
@@ -23,4 +29,19 @@ import org.springframework.context.annotation.Configuration
 @Suppress("unused")
 // Set Swagger Config Class
 @Configuration
-class SwaggerConfig
+class SwaggerConfig {
+    @Bean
+    // Function that allows to Set new Props Values in Open API
+    fun customOpenAPI(): OpenApiCustomizer {
+        // Returns OpenAPI Customizer with new Props
+        return OpenApiCustomizer { openApi: OpenAPI ->
+            // Get a Schemas Map from OpenAPI
+            val schemas: Map<String, Schema<*>>? = openApi.components?.schemas
+            // If any Schema exist, pass
+            if (schemas != null) {
+                // Order Schemas by Name using TreeMap to keep the keys in natural mode, in this case alphabetically
+                openApi.components.schemas = TreeMap(schemas)
+            }
+        }
+    }
+}
